c573adbf12ecedc8064607805a9b0388496a3795
tsmit143 <148919767+tsmit143@users.noreply.github.com>
2024-01-02 21:57:56 -0500

    added icons

--- script.js
+++ script.js
@@ -3,15 +3,15 @@ var stateInput = document.getElementById('statesearch-bar');
 var cityInput = document.getElementById('search-bar');
 var countryInput = document.getElementById('countrysearch-bar');
 var searchBtn = document.getElementById("search-button");
-var searchList = document.getElementById('search-list');
+var searchListEL = document.getElementById('search-list');
 var currentDay = document.getElementById('currentDay');
 var forecastList = document.getElementById('forecast-list');
 var todayEl = document.getElementById('date');
+var weatherArray = JSON.parse(localStorage.getItem('city')) || [];
 //when button is clicked function for city search commences and then is saved in the history list
 
 function displayWeather (){
     var requestUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + cityInput.value +"," +stateInput.value+","+countryInput.value + "&appid=" + apiKey+"&units=imperial";
-    //console.log(requestUrl);
 
     fetch(requestUrl)
     .then (function(response){
@@ -19,7 +19,7 @@ function displayWeather (){
         return response.json();
     })
     .then(function (data){
-        // console.log(data);
+        $("#currentDay").empty();
         //display city name
         currentDayTitle.textContent = cityInput.value;
         //date
@@ -37,11 +37,13 @@ function displayWeather (){
         var weatherIcon = currentDay.appendChild(document.createElement('li'));
         //weathericon.textContent = "Icon:";
 
-        var iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-        weatherIcon.innerHTML = `Icon: <img scr=${iconUrl} alt=${data.weather[0].description}>`;
+        var iconUrl = document.createElement('img');
+        iconUrl.setAttribute('src',"https://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+       // `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+        weatherIcon.append(iconUrl);// = `Icon: <img scr=${iconUrl} alt=${data.weather[0].description}>`;
     })
     displayForecast();
-    saveSearch();
+    viewCities();
 };
 //search button
 searchBtn.addEventListener('click', displayWeather);
@@ -56,12 +58,9 @@ fetch(requestUrl)
     return response.json();
 })
 .then (function (data){
-    // console.log(data);
 
     var lat = data[0].lat;
     var lon = data[0].lon;
-    //console.log(lat);
-    // console.log(lon);
     var requestUrl2 = "http://api.openweathermap.org/data/2.5/forecast?lat="+lat +"&lon="+lon+"&appid="+apiKey+"&units=imperial";
 
     //receieve 5 day forcast 
@@ -71,35 +70,35 @@ fetch(requestUrl)
         return response.json();
     })
     .then (function(data2){
-    //console.log(data2);
+
     //selects each day of the forecast list
     var filteredList = data2.list.filter(function(value,index,array){return index % 8==0;});
-
+$("#forecast-list").empty();
     for (var i=0; i < filteredList.length; i++){
         //date
         var forecastDate = forecastList.appendChild(document.createElement('li'));
-        console.log(`Date: ${dayjs(filteredList[i].dt_txt).format('MM/DD/YYYY')}`);
         forecastDate.textContent = `Date: ${dayjs(filteredList[i].dt_txt).format('MM/DD/YYYY')}`;
 
         //icon
         var forecastIcon = forecastList.appendChild(document.createElement('li'));
-        console. log(`Icon: https://openweathermap.org/img/wn/${filteredList[i].weather[0].icon}@2x.png`);
-        var iconUrl = `https://openweathermap.org/img/wn/${filteredList[i].weather[0].icon}@2x.png`;
-        forecastIcon.innerHTML = `Icon: <img scr=${iconUrl} alt=${filteredList[i].weather[0].description}>`;
+
+        var iconUrl = document.createElement('img');
+         iconUrl.setAttribute('src', "https://openweathermap.org/img/wn/" + filteredList[i].weather[0].icon + "@2x.png");
+        forecastIcon.append(iconUrl); 
 
         //temp
         var forecastTemp = forecastList.appendChild(document.createElement('li'));
-        console.log(`Temp: ${filteredList[i].main.temp}`);
+
         forecastTemp.textContent = `Temp: ${filteredList[i].main.temp}`;
 
         //wind
         var forecastWind = forecastList.appendChild(document.createElement('li'));
-        console.log(`Wind Speed: ${filteredList[i].wind.speed} mph`);
+
         forecastWind.textContent = `Wind Speed: ${filteredList[i].wind.speed} mph`;
 
         //humidity
         var forecastHumid = forecastList.appendChild(document.createElement('li'));
-        console.log(`Humidity: ${filteredList[i].main.humi} mph`);
+
         forecastHumid.textContent = `Humidity: ${filteredList[i].main.humidity} mph`;
     }
     })
@@ -107,21 +106,24 @@ fetch(requestUrl)
 
 
 }
-//show city searches****
-function saveSearch(){
-event.preventDefault();
-var city = cityInput.value;
-localStorage.setItem('city',JSON.stringify(city));
+
+
+function viewCities(){
+    var newCity = $("#search-bar").val();
+
+    if (!weatherArray.includes(newCity)){
+        weatherArray.push(newCity);
+        localStorage.setItem('city',JSON.stringify(weatherArray));
+    }
+
+  viewSaved();
 };
 
-function printSavedSearch(){
-var lastSearch = JSON.parse(localStorage.getItem('city'));
-if (lastSearch !=- null){
-    
-}
-};
+function viewSaved (){
+$("#search-list").empty();
+    for (var i=0; i < weatherArray.length; i++){
 
-//city search is added to an array
-    //city is saved to local storage
-//then array is printed on screen
-    //grabbed from local storage
+         var savedCities = searchListEL.appendChild(document.createElement('li'));
+         savedCities.textContent = weatherArray[i];
+    }
+};
